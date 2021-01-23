d8fb02aa36e3cee2c517cb421f1ddc0fb065168f
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2021-01-23 14:53:48 -0500

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi hi hi");
+        // git status
+        // git add
+        // git commit -m "msg"
+
+        // git push
+    }
+}
+
